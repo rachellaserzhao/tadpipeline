169d71a7b148932fa8522aedb75e804000c73ff9
rachellaserzhao <ruiqirachelzhao@gmail.com>
2017-10-15 12:58:56 -0700

    ..

--- pipelines_new.py
+++ pipelines_new.py
@@ -62,7 +62,7 @@ def alignment(r1, r2, genome, prefix, outdir):
 def merge(outdir):
 
 	cmd = python + ' ' + hic + 'mergeSAM.py' + ' -f ' + outdir + '/' + 'alignment.R1.bam'+ \
-	+ ' -r ' + outdir + '/' + 'alignment.R2.bam' + ' -o ' + outdir + '/' + 'pairs.bam'
+	' -r ' + outdir + '/' + 'alignment.R2.bam' + ' -o ' + outdir + '/' + 'pairs.bam'
 
 	print cmd
 
