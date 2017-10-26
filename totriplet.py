40d47a2ad7a4e3e130c615ede02afb8dbd54c950
Rachel Zhao <rachelz@seawolf2.ibb>
2017-10-26 11:40:56 -0700

    parse

--- totriplet.py
+++ totriplet.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.sparse import coo_matrix
+import sys
 
 data = np.loadtxt("/home/rachelz/pipeline/tadpipeline/cluster_test/outputMatrixFile.txt", skiprows=1, usecols=range(2,325))
 triplet = coo_matrix(data)
@@ -7,6 +8,7 @@ triplet = coo_matrix(data)
 newrow = [x+1 for x in triplet.row]
 newcol = [x+1 for x in triplet.col]
 
-with open ('trip.txt' , 'a') as output:
+with open ('trip.txt' , 'w') as output:
+	sys.stdout = output
 	for r, v, c in zip(newrow, triplet.data, newcol):
-		output.write("{0}\t{1}\t{1}".format(r, v, c))
+		print("{0}\t{1}\t{1}".format(r, v, c))
